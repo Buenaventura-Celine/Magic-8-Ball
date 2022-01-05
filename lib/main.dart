ad0801b120a1285c140758949cc0cee36ada5c57
Celine Joie V. Buenaventura <buenaventuraceline277@gmail.com>
2022-01-05 11:12:40 +0800

    Created stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,40 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() {
+  return runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          title: Center(child: Text('Ask Me Anything...')),
+          backgroundColor: Colors.blueAccent,
+        ),
+        body: BallPage(),
+      ),
+    ),
+  );
+}
+
+class BallPage extends StatefulWidget {
+  @override
+  _BallPageState createState() => _BallPageState();
+}
+
+class _BallPageState extends State<BallPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        child: Expanded(
+          child: TextButton(
+            onPressed: () {
+              print('Magic 8 ball was pressed');
+            },
+            child: Image.asset('images/ball1.png'),
+          ),
+        ),
       ),
     );
+  }
+}
